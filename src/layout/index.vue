254a5c474701d9b294bc6d60d3ce663db4c32baa
bidianqing <bidianqing@qq.com>
2023-12-18 21:01:00 +0800

    init

---
+++ src/layout/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-container>
+            <el-aside>
+                <el-menu
+                    background-color="#545c64"
+                    text-color="#fff">
+                    <el-menu-item index="/">Hello</el-menu-item>
+                    <el-menu-item index="/news">News</el-menu-item>
+                    <el-menu-item index="/about">About</el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-container>
+                <el-header>
+                    <el-radio-group style="margin-bottom: 20px">
+                        <el-radio-button :label="false">expand</el-radio-button>
+                        <el-radio-button :label="true">collapse</el-radio-button>
+                    </el-radio-group>
+                </el-header>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Layout from '~/layout/index.vue'
+import HelloWorld from '~/components/HelloWorld.vue'
+import Index from '~/views/index.vue'
+import About from '~/views/about.vue'
+import NotFound from '~/views/404.vue'
+import Login from '~/views/login.vue'
+import News from '~/views/news.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: Layout
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: NotFound,
+        component: NotFound
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/hello',
+        component: Layout,
+        children: [
+            {
+              path: '',
+              component: HelloWorld
+            }
+        ]
+    },
+    {
+        path: '/about',
+        component: Layout,
+        children: [
+            {
+              path: '/about',
+              component: About
+            }
+        ]
+    },
+    {
+        path: '/news',
+        component: Layout,
+        children: [
+            {
+              path: '/news',
+              component: News
+            }
+        ]
+    }
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
