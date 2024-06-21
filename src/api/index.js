13b3394dcf77cb7359cb076a3b62a21b8b0d09a3
bidianqing <bidianqing@qq.com>
2024-06-21 16:46:55 +0800

    *

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,13 @@
 import httpClient from '@/utils/request'
 
+export async function login(data){
+    return await httpClient({
+        method: 'post',
+        url: '/login',
+        data
+    })
+}
+
 export async function getUsers(){
     return await httpClient({
         method: 'get',
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: ''
+    baseURL: 'http://localhost:8080'
 
+})
+
+httpClient.interceptors.request.use(function(config){
+    return config;
+},function(error){
+    return Promise.reject(error);
 })
 
 export default httpClient
